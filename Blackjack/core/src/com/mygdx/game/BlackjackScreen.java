e552db14d1470bef8028bb15357d2dedaae11b67
TheInstantGamer <nkstevo@gmail.com>
2016-09-03 21:38:18 -0400

    Added Card.java and added card drawing functionality
    Added the Card class which will allows me to store the different
    blackjack values as well as the suit of the cards. Also added an
    ArrayList of Cards which will hold the entire deck of 52 cards. Cards
    are loaded in via the loadCards() function in BlackjackScreen. Currently
    the game just loads in a random card from the deck.

--- Blackjack/core/src/com/mygdx/game/BlackjackScreen.java
+++ Blackjack/core/src/com/mygdx/game/BlackjackScreen.java
@@ -1,5 +1,8 @@
 package com.mygdx.game;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,16 +14,27 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class BlackjackScreen implements Screen {
-	CardGame game;
+	private CardGame game;
 	
-	SpriteBatch batch;
-	BitmapFont font;
+	private SpriteBatch batch;
+	private BitmapFont font;
 	
-	Texture cardSheet;
-	TextureRegion card;
-	Sprite cardSprite;
+	private Texture cardSheet;
+	private TextureRegion card;
+	private Sprite cardSprite;
 	
 	private OrthographicCamera camera;
+	
+	private ArrayList<Card> cardDeck;
+	
+	/**Each ArrayList of Cards is a player's hand
+	 * The ArrayList at index 0 is the dealer's hand
+	 */
+	private ArrayList<ArrayList<Card>> playerHands;
+
+	
+	private int cardInd;
+	private Card testCard;
 	
 	public BlackjackScreen(CardGame game) {
 		this.game = game;
@@ -30,14 +44,17 @@ public class BlackjackScreen implements Screen {
 		
 		camera = new OrthographicCamera();
 		
-		//cards are 140x190
-		cardSheet = new Texture(Gdx.files.internal("spritesheets\\playingCards.png"));
-		card = new TextureRegion(cardSheet, 0, 0, 140, 190);
-		cardSprite = new Sprite(card);
+		Random randGen = new Random();
+		cardInd = randGen.nextInt(51);
 		
-		cardSprite.setScale(0.5f);
-		cardSprite.setCenterX(200);
-		cardSprite.setCenterY(200);
+		this.loadCards();
+		
+		testCard = cardDeck.remove(cardInd);
+		
+		System.out.println(testCard.getSuit());
+		System.out.println(testCard.getValue());
+		System.out.println(testCard.getAltVal(0));
+		
 	}
 	
 	@Override
@@ -48,12 +65,13 @@ public class BlackjackScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
 		Gdx.gl.glClearColor(6f/255f, 156f/255f, 36f/255f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
-		cardSprite.draw(batch);
+		
+		testCard.getSprite().draw(batch);	
+		
 		batch.end();
 	}
 
@@ -87,4 +105,85 @@ public class BlackjackScreen implements Screen {
 		game.dispose();
 	}
 
+	public void loadCards() {
+		TextureRegion card;
+		Sprite cardSprite;
+		
+		int row = 9;
+		
+		int width = 140;
+		int height = 190;
+		
+		int xPos = 0;
+		int yPos = 0;
+		
+		int value = 10;
+		int altVal = 1;
+		
+		int valKey;
+		int suitKey;
+		
+		Card.Suit suit;
+		
+		cardSheet = new Texture(Gdx.files.internal("spritesheets\\playingCards.png"));
+		cardDeck = new ArrayList<Card>();
+		
+		for (int i = 0; i < 52; i++, yPos += height) {
+			if (xPos == 140 && yPos == height * 3) {
+				i--;
+				continue;
+			}
+			
+			card = new TextureRegion(cardSheet, xPos, yPos, width, height);
+			cardSprite = new Sprite(card);
+			
+			cardSprite.setScale(0.5f);
+			cardSprite.setCenter(400, 300);
+			
+			//Keeps track of which card in the suit is being loaded
+			valKey = i % 13;
+			
+			if (valKey >= 0 && valKey < 3) {
+				value = 10;
+			} else if (valKey == 3) {
+				value = 11;
+				altVal = 1;
+			} else {
+				value--;
+			}
+			
+			//Keeps track of the suit of the card being loaded
+			suitKey = i / 13;
+			
+			if (suitKey < 1) {
+				suit = Card.Suit.SPADES;
+			} else if (suitKey == 1) {
+				suit = Card.Suit.HEARTS;
+			} else if (suitKey == 2) {
+				suit = Card.Suit.DIAMONDS;
+			} else {
+				suit = Card.Suit.CLUBS;
+			}
+			
+			//A special exception for two cards on the spritesheet which are out of place
+			if (valKey == 12 && suitKey == 1) {
+				suit = Card.Suit.CLUBS;
+			} else if (valKey == 12 && suitKey > 2) {
+				suit = Card.Suit.HEARTS;
+			}
+			
+			//If the card is an ace, it uses the alt value constructor
+			if (valKey == 3) {
+				cardDeck.add(new Card(cardSprite, suit, value, altVal));
+			} else {
+				cardDeck.add(new Card(cardSprite, suit, value));
+			}
+			
+			//Moves to the next column of the sprite sheet after reaching the 10th row
+			if (yPos >= height * row) {
+				yPos = -height;
+				xPos += width;
+			}
+		}
+	}
 }
